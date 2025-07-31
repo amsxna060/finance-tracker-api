07dc3ff18bae9334d2b288bfb7595f10d5a50d2c
amolsaxena060 <amolsaxena060@gmail.com>
2025-07-31 19:32:22 +0530

    Add healthcheck and Version

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/")
 async def first_step():
     return {"message": "Hello Finance World!"}
+
+@app.get("/health")
+def health_check():
+    return {
+        "timestamp": datetime.now().isoformat(),
+        "status": "Api is Running"
+    }
+
+@app.get("/version")
+def get_version():
+    return {
+        "version" : "1.0.0",
+        "description": "This is the version endpoint of the Finance API."
+    }
+ 
